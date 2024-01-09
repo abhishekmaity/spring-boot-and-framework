284d9cffaa2cb67486eb69a93a5d6ff38c53c1d0
abhishekmaity <abhishek.maity@hotmail.com>
2024-01-09 23:15:08 +0530

    first commit

---
+++ src/test/java/in/abhishek/springboot/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.abhishek.springboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
